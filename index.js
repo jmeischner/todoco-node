3f9d597fff027c0ec5ad0acc75cffd891ee8b066
Jan Meischner <jan.meischner@adesso.de>
2017-03-06 23:15:41 +0100

    Finds todo comments in sourcefile

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 var fs = require('fs');
-var Rx = require('rxjs');
+var Rx = require('rxjs/Rx');
 var glob = require('globby');
 var _ = require('lodash');
+
+var todoRegex = /todo:.*/ig;
 
 Rx.Observable.fromPromise(glob(['**/*.js', '!node_modules/**/*']))
     .flatMap(path => Rx.Observable.from(path))
     .map(extractTodo)
-    .map(file => {
-        var content = '';
-        file.datastream.on('data', (d) => {
-            content += d;
+    .subscribe(file => {
+        file.datastream.subscribe(data => {
+            console.log("Path: " + file.path + "\nContent:\n" + data.match(todoRegex));
+        }, error => {
+            console.error(error);
         });
-        
-        Rx.Observable.fromEvent(file.datastream, 'data').reduce((acc, x, idx, source) => {
-            return acc + x;
-        }).subscribe(x => console.log(x));
-    }).subscribe(x => {
-        console.log(x);
     });
 
 function extractTodo(path) {
+    var datastream = Rx.Observable.bindNodeCallback(fs.readFile);
     return {
         path: path,
-        datastream: fs.createReadStream(path, {
-          flags: 'r',
-          encoding: 'utf8',
-          fd: null,
-          mode: 0o666,
-          autoClose: true
-      })
+        datastream: datastream(path, 'utf8')
     };
 }
 
