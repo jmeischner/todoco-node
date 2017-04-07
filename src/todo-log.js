7b95bef0ef87413ae98e4e6c649f24aa07865ed0
Jan Meischner <jan.meischner@adesso.de>
2017-04-07 23:25:50 +0200

    Added styled error log

---
+++ src/todo-log.js
@@ -0,0 +1,19 @@
+let colors = require('colors');
+
+const errorLog = function(code, message, details=false) {
+    const errorCode = "[" + code + "]";
+    let error = "";
+    if (details) {
+        error = errorCode.bold.red + ": ".cyan + message.red.bold + "\n" + "  Details".bold + ": ".cyan + details;
+    } else {
+        error = errorCode.bold.red + ": ".cyan + message.red.bold;
+    }
+    console.log(error);
+
+    return null;
+};
+
+
+module.exports = {
+    error: errorLog
+};
