9399de7d835b17ae4fbde9be95c7877db044929b
Jan Meischner <jan.meischner@adesso.de>
2017-05-09 22:10:32 +0200

    Added emtpy ReadMe.md

--- todoco.js
+++ todoco.js
@@ -1,7 +1,7 @@
 const program = require('commander');
 
 program
-    .version('0.0.1')
+    .version('0.0.2')
     .command('list', 'List ToDos of current project', {isDefault: true})
     .command('init', 'Initiate a ToDoCo Project. Results in a .todoco config file.')
     .command('push', 'Push ToDos to the configured Server')
