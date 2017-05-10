b02931dbfdc1726d4a24da5d581e6ba4da543072
Jan Meischner <jan.meischner@adesso.de>
2017-05-10 21:55:16 +0200

    Added --path option to list command

--- todoco-list.js
+++ todoco-list.js
@@ -1,13 +1,35 @@
 const todoReader = require('./src/extracting/reader');
 const configReader = require('./src/config/config');
 const list = require('./src/logging/list-todos');
+const program = require('commander');
+const path = require('path');
 
-const config = configReader.readConfig('.');
-const files = configReader.getFiles('.', config.files);
+program
+    .option('-p, --path <path>', 'List Todos of files in given path')
+    .parse(process.argv);
 
-const clear = require('clear');
-clear();
+if (program.path) {
+    let filesPath = path.join(program.path, '**');
+    const filesConfig = {
+        add: [filesPath],
+        ignore: ['**'],
+        useGitignore: false
+    };
 
-todoReader(files).subscribe(file => {
-    list(file);
-});
+    const files = configReader.getFiles('', filesConfig);
+    readTodosFromFiles(files);
+
+} else {
+    const config = configReader.readConfig('.');
+    const files = configReader.getFiles('.', config.files);
+    readTodosFromFiles(files);
+}
+
+function readTodosFromFiles(files){
+    const clear = require('clear');
+    clear();
+
+    todoReader(files).subscribe(file => {
+        list(file);
+    });
+}
